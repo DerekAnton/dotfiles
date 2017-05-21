6571f5490183523fefa3c27eeee604661ebb4ccc
Danton <Danton@localhost.localdomain>
2017-05-21 16:15:05 -0400

    Added wifi-fetch script
    wrote a small script to find the wireless network with the highest signal
    strength and then attempt to connect to it, given a password as a command line parameter.

---
+++ wifi-fetch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Finds the wireless network with the highest signal and returns it's SSID
+
+nmcli -f ssid,signal device wifi list | tr -s " " > ~/wifilist.dump
+
+highestSignal=0
+highestSSID="NULL"
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	stringarray=($line)
+	if [ ${stringarray[0]} != '--' ] && [ ${stringarray[0]} != 'SIGNAL' ]; then
+	        if (( ${stringarray[1]} > $highestSignal )); then 
+                        highestSSID="${stringarray[0]}"
+			let highestSignal="${stringarray[1]}"
+		fi
+	fi
+done < ~/wifilist.dump 
+
+echo determined $highestSSID has a signal strength of $highestSignal%
+echo attempting to connect...
+
+# Cleanup
+rm ~/wifilist.dump
+
+# Attempt to connect to the network with the highest connect with password passed as a parameter
+nmcli device wifi connect $highestSSID password $1
+
