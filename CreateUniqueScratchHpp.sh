c4b2e9aba33ca206014c9cd43f11a02c5017ed61
Derek Anton <danton5276@gmail.com>
2017-06-12 13:39:47 -0400

    Create CreateUniqueScratchHpp.sh

--- CreateUniqueScratchHpp.sh
+++ CreateUniqueScratchHpp.sh
@@ -1,11 +1,10 @@
-
 name=scratch
- 
-if [[ -e $name.hpp ]] ; then
+
+if [[ -e $name.cpp ]] ; then
     i=$((1 + RANDOM % 999))
-    while [[ -e $name-$i.hpp ]] ; do
+    while [[ -e $name-$i.cpp ]] ; do
         let i++
     done
-  name=$name-$i
+    name=$name-$i
 fi
-vim ~/scratch/"$name".hpp
+vim ~/scratch/"$name".cpp
