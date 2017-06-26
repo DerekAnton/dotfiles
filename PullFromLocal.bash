cc2f164e09b55e844afeb5ae35dac637b81c491e
Derek Anton <danton5276@gmail.com>
2017-06-26 08:31:28 -0400

    Update PullFromLocal.bash

---
+++ PullFromLocal.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Hard coded copies, use to check against PushFromRepo
+
+# Dotfiles
+cp ~/.bash_aliases ~/git/Bash_Scripts/dotfiles/.bash_aliases 
+cp ~/.bashrc ~/git/Bash_Scripts/dotfiles/.bashrc
+cp ~/.gitconfig ~/git/Bash_Scripts/dotfiles/.gitconfig
+cp ~/.template ~/scratch/.template
+cp ~/.vimrc ~/git/Bash_Scripts/dotfiles/.vimrc
+
+# I3 config
+cp ~/.config/i3/config ~/git/Bash_Scripts/dotfiles/config/i3/config 
+
+# Userscripts
+cp ~/.userscripts/CreateUniqueScratchCpp.bash ~/git/Bash_Scripts/userscripts/CreateUniqueScratchCpp.bash
+cp ~/.userscripts/NoPassWifiFetch.bash ~/git/Bash_Scripts/userscripts/NoPassWifiFetch.bash
+cp ~/.userscripts/WifiFetch.bash ~/git/Bash_Scripts/userscripts/WifiFetch.bash
