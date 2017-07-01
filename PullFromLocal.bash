2853bc53ebc8f4f3fd1689865c56fcbbf09ef6be
DerekAnton <danton5276@gmail.com>
2017-06-30 20:39:41 -0400

    Functional PullFromLocal and other small changes

--- PullFromLocal.bash
+++ PullFromLocal.bash
@@ -3,16 +3,16 @@
 # Hard coded copies, use to check against PushFromRepo
 
 # Dotfiles
-cp ~/.bash_aliases ~/git/Bash_Scripts/dotfiles/bash_aliases 
-cp ~/.bashrc ~/git/Bash_Scripts/dotfiles/bashrc
-cp ~/.gitconfig ~/git/Bash_Scripts/dotfiles/gitconfig
-cp ~/scratch/.template ~/git/Bash_Scripts/dotfiles/template
-cp ~/.vimrc ~/git/Bash_Scripts/dotfiles/vimrc
+cp ~/.bash_aliases ~/git/Bash_Scripts/dotfiles/bash_aliases &&
+cp ~/.bashrc ~/git/Bash_Scripts/dotfiles/bashrc &&
+#cp ~/.gitconfig ~/git/Bash_Scripts/dotfiles/gitconfig
+cp ~/scratch/.template ~/git/Bash_Scripts/dotfiles/template &&
+cp ~/.vimrc ~/git/Bash_Scripts/dotfiles/vimrc &&
 
 # I3 config
-cp ~/.config/i3/config ~/git/Bash_Scripts/dotfiles/config/i3/config 
+cp ~/.config/i3/config ~/git/Bash_Scripts/dotfiles/config/i3/config &&
 
 # Userscripts
-cp ~/.userscripts/CreateUniqueScratchCpp.bash ~/git/Bash_Scripts/userscripts/CreateUniqueScratchCpp.bash
-cp ~/.userscripts/NoPassWifiFetch.bash ~/git/Bash_Scripts/userscripts/NoPassWifiFetch.bash
+cp ~/.userscripts/CreateUniqueScratchCpp.bash ~/git/Bash_Scripts/userscripts/CreateUniqueScratchCpp.bash &&
+cp ~/.userscripts/NoPassWifiFetch.bash ~/git/Bash_Scripts/userscripts/NoPassWifiFetch.bash &&
 cp ~/.userscripts/WifiFetch.bash ~/git/Bash_Scripts/userscripts/WifiFetch.bash
