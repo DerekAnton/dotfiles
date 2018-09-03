d0b91c1cda8aab186b14006572c4770e760506f5
DerekAnton <danton5276@gmail.com>
2018-09-03 13:23:33 -0400

    Simplified Pull and Push
        ∙ PullFromLocal
          - renamed assumed directory to ~/git/dotfiles to match git repo
            name
        ∙ PushFromRepo
          - changed to hardcode copies instead of search repo directory
        ∙ bash_aliases
          - made git grep case insensitive by default

--- PullFromLocal.bash
+++ PullFromLocal.bash
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-# Hard coded copies, use to check against PushFromRepo
-
 # Dotfiles
-cp ~/.bash_aliases ~/git/Bash_Scripts/dotfiles/bash_aliases &&
-echo "wrote ~/.bash_aliases to ~/git/Bash_Scripts/dotfiles/bash_aliases"
-cp ~/.bashrc ~/git/Bash_Scripts/dotfiles/bashrc &&
-echo "wrote ~/.bashrc to ~/git/Bash_Scripts/dotfiles/bashrc"
-#cp ~/.gitconfig ~/git/Bash_Scripts/dotfiles/gitconfig
-cp ~/scratch/.template ~/git/Bash_Scripts/dotfiles/template &&
-echo "wrote ~/scratch/.template to ~/git/Bash_Scripts/dotfiles/template"
-cp ~/.vimrc ~/git/Bash_Scripts/dotfiles/vimrc &&
-echo "wrote ~/.vimrc to ~/git/Bash_Scripts/dotfiles/vimrc"
+cp ~/.bash_aliases ~/git/dotfiles/dotfiles/bash_aliases
+echo "wrote ~/.bash_aliases to ~/git/dotfiles/dotfiles/bash_aliases"
+cp ~/.bashrc ~/git/dotfiles/dotfiles/bashrc
+echo "wrote ~/.bashrc to ~/git/dotfiles/dotfiles/bashrc"
+#cp ~/.gitconfig ~/git/dotfiles/dotfiles/gitconfig
+cp ~/scratch/.template ~/git/dotfiles/dotfiles/template
+echo "wrote ~/scratch/.template to ~/git/dotfiles/dotfiles/template"
+cp ~/.vimrc ~/git/dotfiles/dotfiles/vimrc
+echo "wrote ~/.vimrc to ~/git/dotfiles/dotfiles/vimrc"
 
 # I3 config
-cp ~/.config/i3/config ~/git/Bash_Scripts/dotfiles/config/i3/config &&
-echo "wrote ~/.config/i3/config to ~/git/Bash_Scripts/dotfiles/config/i3/config"
-cp /etc/i3status.conf ~/git/Bash_Scripts/etc/i3status.conf &&
-echo "wrote /etc/i3status.conf to ~/git/Bash_Scripts/etc/i3status.conf"
-
+cp ~/.config/i3/config ~/git/dotfiles/dotfiles/config/i3/config
+echo "wrote ~/.config/i3/config to ~/git/dotfiles/dotfiles/config/i3/config"
+cp /etc/i3status.conf ~/git/dotfiles/etc/i3status.conf
+echo "wrote /etc/i3status.conf to ~/git/dotfiles/etc/i3status.conf"
 
 # Userscripts
-cp ~/.userscripts/CreateUniqueScratchCpp.bash ~/git/Bash_Scripts/userscripts/CreateUniqueScratchCpp.bash &&
-echo "wrote ~/.userscripts/CreateUniqueScratchCpp.bash to ~/git/Bash_Scripts/userscripts/CreateUniqueScratchCpp.bash"
-cp ~/.userscripts/NoPassWifiFetch.bash ~/git/Bash_Scripts/userscripts/NoPassWifiFetch.bash &&
-echo "wrote ~/.userscripts/NoPassWifiFetch.bash to ~/git/Bash_Scripts/userscripts/NoPassWifiFetch.bash"
-cp ~/.userscripts/WifiFetch.bash ~/git/Bash_Scripts/userscripts/WifiFetch.bash
-echo "wrote ~/.userscripts/WifiFetch.bash to ~/git/Bash_Scripts/userscripts/WifiFetch.bash"
+cp ~/.userscripts/CreateUniqueScratchCpp.bash ~/git/dotfiles/userscripts/CreateUniqueScratchCpp.bash
+echo "wrote ~/.userscripts/CreateUniqueScratchCpp.bash to ~/git/dotfiles/userscripts/CreateUniqueScratchCpp.bash"
+cp ~/.userscripts/NoPassWifiFetch.bash ~/git/dotfiles/userscripts/NoPassWifiFetch.bash
+echo "wrote ~/.userscripts/NoPassWifiFetch.bash to ~/git/dotfiles/userscripts/NoPassWifiFetch.bash"
+cp ~/.userscripts/WifiFetch.bash ~/git/dotfiles/userscripts/WifiFetch.bash
+echo "wrote ~/.userscripts/WifiFetch.bash to ~/git/dotfiles/userscripts/WifiFetch.bash"
--- PushFromRepo.bash
+++ PushFromRepo.bash
@@ -1,64 +1,16 @@
 #!/bin/bash
 
-filename="null"
-
 # Dotfiles
-for file in ~/git/Bash_Scripts/dotfiles/*; do
+cp ~/git/dotfiles/dotfiles/bash_aliases ~/.bash_aliases
+cp ~/git/dotfiles/dotfiles/bashrc ~/.bashrc
+cp ~/git/dotfiles/dotfiles/template ~/scratch/.template
+cp ~/git/dotfiles/dotfiles/vimrc ~/.vimrc
 
-    # file returns the full path to that file
-    # the following command removes all characters up until the last "/" from file and prepends a "."
-    filename=".${file##*/}"
-
-    if [ $filename == ".bash_aliases" ]
-    then
-        echo "writting " $filename " to ~/"
-        cp $file ~/$filename
-    elif [ $filename == ".bashrc" ]
-    then
-        echo "writting " $filename " to ~/"
-        cp $file ~/$filename
-    elif [ $filename == ".gitconfig" ]
-    then
-        echo "skipping " $filename " to ~/"
-        #cp $file ~/$filename
-    elif [ $filename == ".template" ]
-    then
-        echo "writting " $filename " to ~/scratch/"
-        cp $file ~/scratch/$filename
-    elif [ $filename == ".vimrc" ]
-    then
-        echo "writting " $filename " to ~/"
-        cp $file ~/$filename
-    else
-        echo "Unknown file name: " $filename
-    fi
-done
-
-echo "Writting i3 config to ~/.config/i3/config"
-cp ~/git/Bash_Scripts/dotfiles/config/i3/config ~/.config/i3/config
-
+# I3 config
+cp ~/git/dotfiles/dotfiles/config/i3/config ~/.config/i3/config
+cp ~/git/dotfiles/etc/i3status.conf /etc/i3status.conf
 
 # Userscripts
-for file in ~/git/Bash_Scripts/userscripts/*; do
-
-    filename="${file##*/}"
-
-    if [ $filename == "CreateUniqueScratchCpp.bash" ]
-    then
-        echo "writting " $filename " to ~/.userscripts"
-        cp $file ~/.userscripts/$filename
-
-    elif [ $filename == "NoPassWifiFetch.bash"  ]
-    then
-        echo "writting " $filename " to ~/.userscripts"
-        cp $file ~/.userscripts/$filename
-
-    elif [ $filename == "WifiFetch.bash"  ]
-    then
-        echo "writting " $filename " to ~/.userscripts/"
-        cp $file ~/.userscripts/$filename
-
-    else
-        echo "Unknown file name: " $filename
-    fi
-done
+cp ~/git/dotfiles/userscripts/CreateUniqueScratchCpp.bash ~/.userscripts/CreateUniqueScratchCpp.bash
+cp ~/git/dotfiles/userscripts/NoPassWifiFetch.bash ~/.userscripts/NoPassWifiFetch.bash
+cp ~/git/dotfiles/userscripts/WifiFetch.bash ~/.userscripts/WifiFetch.bash
